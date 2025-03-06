a8d1c87f86cdf41c29850c3e04ed7e3371428f42
MattSouza14 <mateussouzabr1@gmail.com>
2025-03-06 10:13:56 -0300

    First commit

---
+++ src/main/java/com/example/ApiSistemaCadastro/ApiSistemaCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiSistemaCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSistemaCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApiSistemaCadastro/ApiSistemaCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiSistemaCadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSistemaCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
